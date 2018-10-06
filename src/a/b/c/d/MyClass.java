764c8ece081edf577d2385e60fad633544968ed2
balajakka <jbkrao8281@gmail.com>
2018-10-06 17:58:23 +0100

    Created PackagesAndImports

---
+++ src/a/b/c/d/MyClass.java
@@ -0,0 +1,7 @@
+package a.b.c.d;
+
+/**
+ * Created by balajakka on 06/10/2018.
+ */
+public class MyClass {
+}
---
+++ src/academy/learnprogramming/ImportExample.java
@@ -0,0 +1,15 @@
+package academy.learnprogramming;
+
+import java.util.Random;
+
+/**
+ * Created by balajakka on 06/10/2018.
+ */
+public class ImportExample {
+    public static void main(String[] args) {
+        Random random = new Random();
+        System.out.println(random.nextInt(10));
+
+    }
+
+}
